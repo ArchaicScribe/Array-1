301d51c04bdc2f7ad52bedf05020dcf77f455e0a
Alex Rauenzahn <xandermv@yahoo.com>
2021-06-02 21:25:54 +0000

    Added performance measurement

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const sauron = ['sauron'];
+const {performance} = require('perf_hooks');
 
 function foundYou(array){
+  let t0 = performance.now();
   for (let i = 0; i < array.length; i ++){
     if (array[i] == 'sauron'){
       console.log('Sauron has found you!');
     }
   }
+  let t1 = performance.now();
+  console.log('Call for Sauron to find you took ' + (t1-t0) + ' milliseconds');
 }
 foundYou(sauron);
