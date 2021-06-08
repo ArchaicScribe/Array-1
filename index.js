943a89b93b6d21c404dbec1ed30b70411e813d57
Alex Rauenzahn <>
2021-06-08 22:15:54 +0000

    Added O(1) - Constant Time

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
-const sauron = ['sauron'];
-const everyone = ['sauron' , 'lurtz', 'witch-king of angmar', 'khamul', 'ulaire toldea', 'balrog', 'morgoth', 'shelob',
-'saruman', 'ulaire lemenya'];
-const large = new Array(100000).fill('sauron');
-const {performance} = require('perf_hooks');
+// const sauron = ['sauron'];
+// const everyone = ['sauron' , 'lurtz', 'witch-king of angmar', 'khamul', 'ulaire toldea', 'balrog', 'morgoth', 'shelob',
+// 'saruman', 'ulaire lemenya'];
+// const large = new Array(100000).fill('sauron');
+// const {performance} = require('perf_hooks');
 
-function foundYou(array){
-  let t0 = performance.now();
-  for (let i = 0; i < array.length; i ++){
-    if (array[i] == 'sauron'){
-      console.log('Sauron has found you!');
-    }
-  }
-  let t1 = performance.now();
-  console.log('Call for Sauron to find you took ' + (t1-t0) + ' milliseconds');
+// function foundYou(array){
+//   for (let i = 0; i < array.length; i ++){
+//     if (array[i] == 'sauron'){
+//       console.log('Sauron has found you!');
+//     }
+//   }
+// }
+// foundYou(large); //O(n) or linear time
+const boxes = [0,1,2,3,4,5];
+function logFirstTwoBoces(boxes){
+  console.log(boxes[0]);//O(1)
+  console.log(boxes[1]);//O(1);
 }
-foundYou(large);
+logFirstTwoBoces(boxes); // O (2)
